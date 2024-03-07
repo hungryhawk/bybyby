935fd7c2914ea40f16ecb8363e6c8bd37b995fcb
Pavel Emelianov <emeliarulit@gmail.com>
2024-03-08 01:11:32 +0300

    feat: add storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,7 @@
 module.exports = {
     root: true,
     env: { browser: true, es2020: true, node: 'current', 'jest/globals': true },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:prettier/recommended',
-        'plugin:jest/recommended',
-    ],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:prettier/recommended', 'plugin:jest/recommended', 'plugin:storybook/recommended'],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
     plugins: ['react-refresh', 'jest'],
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/700.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
